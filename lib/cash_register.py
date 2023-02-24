6217db898ae28ea338c11308b5126910d93ade23
triciahughes <hughes.a.tricia@gmail.com>
2023-02-24 09:48:09 -0800

    tests passing but not all

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -4,8 +4,39 @@ class CashRegister:
     def __init__(self, discount=0, total=0):
         self.discount = discount
         self.total = total
+        self.items = []
 
+
+    # items = []
     def add_item(self, item, price, quantity=1):
         self.quantity = quantity
         self.total += price*quantity
+        for i in range(quantity):
+            self.items.append(item)
 
+        # self.items.append(item)
+        return self.items
+
+        
+
+
+    def apply_discount(self, discount=0):
+
+        if self.discount == 0:
+            print("There is no discount to apply.")
+        else:
+            discount_conversion = int((self.discount / 100) * self.total)
+            self.total -= discount_conversion
+            # round(self.total, 0)
+            print(f"After the discount, the total comes to ${self.total}.")
+
+
+# register = CashRegister()
+# register.add_item("cat", 1200)
+# register.add_item("cat", 1200)
+# register.add_item("cat", 1200)
+# register.apply_discount(20)
+
+# new_register = CashRegister()
+# new_register.add_item("eggs", 1.99, 20)
+# new_register.add_item("tomato", 1.76)
