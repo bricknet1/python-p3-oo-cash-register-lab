8f7c9f9b9d9c384cac2a61d4186ca66528347194
Nick Johnson <nlj222@gmail.com>
2023-02-24 08:28:34 -0800

    finished 5 deliverables

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0, total=0):
+        self.discount = discount
+        self.total = total
+
+    def add_item(self, item, price, quantity=1):
+        self.quantity = quantity
+        self.total += price*quantity
+
